1faba93ddbbe102686774430a76a38ec098f935f
Judge-Paul <judgepaulogebe3@gmail.com>
2023-05-10 21:56:10 +0100

    fix: auth context error

--- src/store/AuthContext.jsx
+++ src/store/AuthContext.jsx
@@ -41,7 +41,7 @@ export default function AuthProvider(props) {
       })})
     }
     useEffect(() => {
-      if (NFTsData.length === 0 && (sessionStorage.getItem('NFTsData') === undefined || []) === true) {
+      if (NFTsData.length === 0 && (sessionStorage.getItem('NFTsData') === null)) {
         fetch('https://us-central1-nft-market-cdc31.cloudfunctions.net/api/tokens')
           .then(response => response.json())
           .then(data => {
