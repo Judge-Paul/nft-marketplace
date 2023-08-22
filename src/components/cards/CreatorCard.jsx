e136c2f7e8c1f9e175f7c55e4125c01e4dd476b0
Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
2023-08-22 05:32:23 +0100

    Update CreatorCard.jsx

--- src/components/cards/CreatorCard.jsx
+++ src/components/cards/CreatorCard.jsx
@@ -6,7 +6,7 @@ import { formatNum } from "../../libs/Functions";
 export default function CreatorCard({id, position, avatar, name, sales}) {
     return (
         <motion.div whileHover={{ scale: 0.92 }} className="bg-[#3B3B3B] px-3 py-5 rounded-2xl flex xl:block xl:w-full">
-            <Link to={`/collection/${id}`}>
+            <Link to={`/collection/${slug}`}>
                 <div className="xl:mb-5 flex xl:pr-7 xl:justify-center static w-72">
                     <h4 className="text-white text-xs bg-[#2B2B2B] rounded-full absolute xl:static xl:h-full px-2 xl:px-3 xl:py-1 font-spaceMono">{position}</h4>
                     <img src={avatar} className="rounded-full w-14 h-14 xl:w-40 xl:h-40" alt={`${name} icon`} />
