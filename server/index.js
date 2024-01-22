500620efeafe6d804cf19f4a64aad271fed48e12
Judge-Paul <judgepaulogebe3@gmail.com>
2024-01-22 19:05:10 +0100

    feat: cache data on all endpoints

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 const sdk = require("api")("@reservoirprotocol/v3.0#p570filn9a79gt");
+const NodeCache = require("node-cache");
+const cron = require("node-cron");
 require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-// app.use(morgan("dev"));
+app.use(morgan("combined"));
 
 app.use(
   cors({
@@ -17,6 +19,9 @@ app.use(
 );
 
 sdk.auth(process.env.RESERVOIR_API_KEY);
+
+const collectionsCache = new NodeCache({ stdTTL: 60 * 60 * 24 });
+const tokensCache = new NodeCache({ stdTTL: 60 * 60 * 24 * 7 });
 
 const collectionIds = [
   "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
@@ -31,7 +36,54 @@ const collectionIds = [
   "0xff9c1b15b16263c61d017ee9f65c50e4ae0113d7",
 ];
 
-app.get("/tokens", async (req, res) => {
+async function updateCollectionsCache() {
+  try {
+    // Fetch and cache all-time collections data
+    const allTimeData = await sdk.getCollectionsV7({ accept: "*/*" });
+    collectionsCache.set("allTime", allTimeData.data);
+
+    // Fetch and cache one-day collections data
+    const oneDayData = await sdk.getCollectionsV7({
+      sortBy: "1DayVolume",
+      accept: "*/*",
+    });
+    collectionsCache.set("oneDay", oneDayData.data);
+
+    // Fetch and cache seven-day collections data
+    const sevenDaysData = await sdk.getCollectionsV7({
+      sortBy: "7DayVolume",
+      accept: "*/*",
+    });
+    collectionsCache.set("sevenDays", sevenDaysData.data);
+
+    // Fetch and cache thirty-day collections data
+    const thirtyDaysData = await sdk.getCollectionsV7({
+      sortBy: "30DayVolume",
+      accept: "*/*",
+    });
+    collectionsCache.set("thirtyDays", thirtyDaysData.data);
+
+    console.log("Collections cache updated successfully.");
+  } catch (err) {
+    console.error("Error updating collections cache:", err);
+  }
+}
+
+async function getTokensData(collection) {
+  try {
+    const { data } = await sdk.getTokensV6({
+      collection,
+      limit: 100,
+      accept: "*/*",
+    });
+    return data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+async function cacheTokensData() {
   try {
     const getTokens = async () => {
       return await Promise.all(
@@ -54,7 +106,30 @@ app.get("/tokens", async (req, res) => {
           nft.market.floorAsk.price !== undefined,
       )
       .sort(() => Math.random() - 0.5);
-    res.send(tokens);
+
+    // tokensCache the tokens data with a 12-hour expiration
+    tokensCache.set("tokensCache", tokens, 12 * 60 * 60);
+
+    console.log("Tokens data cached successfully.");
+  } catch (err) {
+    console.error("Error caching tokens data:", err);
+  }
+}
+
+cacheTokensData();
+updateCollectionsCache();
+
+cron.schedule("0 */12 * * *", () => {
+  console.log("Running collections cache update job.");
+  updateCollectionsCache();
+  console.log("Running tokens cache update job.");
+  cacheTokensData();
+});
+
+app.get("/collections", async (req, res) => {
+  try {
+    const collections = collectionsCache.get("allTime");
+    res.send(collections);
   } catch (err) {
     console.error(err);
     res.status(500).send("Internal Server Error");
@@ -62,46 +137,52 @@ app.get("/tokens", async (req, res) => {
 });
 
 app.get("/rankings/oneDay", async (req, res) => {
-  sdk
-    .getCollectionsV7({ sortBy: "1DayVolume", accept: "*/*" })
-    .then(({ data }) => res.send(data))
-    .catch((err) => res.send(err));
+  try {
+    const oneDay = collectionsCache.get("oneDay");
+    res.send(oneDay);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.get("/rankings/sevenDays", async (req, res) => {
-  sdk
-    .getCollectionsV7({ sortBy: "7DayVolume", accept: "*/*" })
-    .then(({ data }) => res.send(data))
-    .catch((err) => res.send(err));
+  try {
+    const sevenDays = collectionsCache.get("sevenDays");
+    res.send(sevenDays);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.get("/rankings/thirtyDays", async (req, res) => {
-  sdk
-    .getCollectionsV7({ sortBy: "30DayVolume", accept: "*/*" })
-    .then(({ data }) => res.send(data))
-    .catch((err) => res.send(err));
-});
-
-app.get("/collections", async (req, res) => {
-  sdk
-    .getCollectionsV7({ accept: "*/*" })
-    .then(({ data }) => res.send(data))
-    .catch((err) => res.send(err));
-});
-
-async function getTokensData(collection) {
   try {
-    const { data } = await sdk.getTokensV6({
-      collection,
-      limit: 100,
-      accept: "*/*",
-    });
-    return data;
+    const thirtyDays = collectionsCache.get("thirtyDays");
+    res.send(thirtyDays);
   } catch (err) {
     console.error(err);
-    return null;
+    res.status(500).send("Internal Server Error");
   }
-}
+});
+
+app.get("/tokens", async (req, res) => {
+  try {
+    const cachedData = tokensCache.get("tokensCache");
+
+    if (cachedData) {
+      console.log("Using cached tokens data.");
+      res.send(cachedData);
+    } else {
+      // If not in cache, fetch data and update cache
+      await cacheTokensData();
+      res.send(tokensCache.get("tokensCache"));
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
