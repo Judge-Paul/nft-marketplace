fe20c67f9e54c829b7529999cb863815f5c60636
Judge-Paul <judgepaulogebe3@gmail.com>
2024-07-14 00:31:34 +0100

    feat: make baseurl interchangeable and create proper error screen

--- client/src/store/DataContext.jsx
+++ client/src/store/DataContext.jsx
@@ -3,86 +3,76 @@ import { useQueries } from "@tanstack/react-query";
 import Spinner from "../components/Spinner";
 
 export const DataContext = createContext({
-  NFTsData: [],
-  collectionsData: [],
-  rankingsOneDay: [],
-  rankingsSevenDays: [],
-  rankingsThirtyDays: [],
+	NFTsData: [],
+	collectionsData: [],
+	rankingsOneDay: [],
+	rankingsSevenDays: [],
+	rankingsThirtyDays: [],
 });
 
 export default function DataProvider({ children }) {
-  const fetchData = async (url) => {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    return response.json();
-  };
-  const queries = [
-    {
-      queryKey: ["NFTs"],
-      queryFn: () =>
-        fetchData(
-          "https://nft-marketplace-be-983ee086ba8a.herokuapp.com/tokens",
-        ),
-    },
-    {
-      queryKey: ["collections"],
-      queryFn: () =>
-        fetchData(
-          "https://nft-marketplace-be-983ee086ba8a.herokuapp.com/collections",
-        ),
-    },
-    {
-      queryKey: ["rankingsOneDay"],
-      queryFn: () =>
-        fetchData(
-          "https://nft-marketplace-be-983ee086ba8a.herokuapp.com/rankings/oneDay",
-        ),
-    },
-    {
-      queryKey: ["rankingsSevenDays"],
-      queryFn: () =>
-        fetchData(
-          "https://nft-marketplace-be-983ee086ba8a.herokuapp.com/rankings/sevenDays",
-        ),
-    },
-    {
-      queryKey: ["rankingsThirtyDays"],
-      queryFn: () =>
-        fetchData(
-          "https://nft-marketplace-be-983ee086ba8a.herokuapp.com/rankings/thirtyDays",
-        ),
-    },
-  ];
+	const fetchData = async (path) => {
+		const baseURL = import.meta.env.VITE_BASE_URL;
+		const response = await fetch(`${baseURL}${path}`);
+		if (!response.ok) {
+			throw new Error("Failed to fetch data");
+		}
+		return response.json();
+	};
+	const queries = [
+		{
+			queryKey: ["NFTs"],
+			queryFn: () => fetchData("/tokens"),
+		},
+		{
+			queryKey: ["collections"],
+			queryFn: () => fetchData("/collections"),
+		},
+		{
+			queryKey: ["rankingsOneDay"],
+			queryFn: () => fetchData("/rankings/oneDay"),
+		},
+		{
+			queryKey: ["rankingsSevenDays"],
+			queryFn: () => fetchData("/rankings/sevenDays"),
+		},
+		{
+			queryKey: ["rankingsThirtyDays"],
+			queryFn: () => fetchData("/rankings/thirtyDays"),
+		},
+	];
 
-  const queryResults = useQueries({ queries: queries });
+	const queryResults = useQueries({ queries: queries });
 
-  if (queryResults.some((result) => result.isLoading)) {
-    return <Spinner />;
-  }
+	if (queryResults.some((result) => result.isLoading)) {
+		return <Spinner />;
+	}
 
-  if (queryResults.some((result) => result.isError)) {
-    return <p>Error fetching data</p>;
-  }
+	if (queryResults.some((result) => result.isError)) {
+		return (
+			<div className="flex h-screen bg-[#2B2B2B] justify-center items-center">
+				<p className="text-white font-bold text-3xl">Error fetching data...</p>
+			</div>
+		);
+	}
 
-  const NFTsData = queryResults[0].data;
-  const collectionsData = queryResults[1].data.collections;
-  const rankingsOneDay = queryResults[2].data.collections;
-  const rankingsSevenDays = queryResults[3].data.collections;
-  const rankingsThirtyDays = queryResults[4].data.collections;
+	const NFTsData = queryResults[0].data;
+	const collectionsData = queryResults[1].data.collections;
+	const rankingsOneDay = queryResults[2].data.collections;
+	const rankingsSevenDays = queryResults[3].data.collections;
+	const rankingsThirtyDays = queryResults[4].data.collections;
 
-  return (
-    <DataContext.Provider
-      value={{
-        NFTsData,
-        collectionsData,
-        rankingsOneDay,
-        rankingsSevenDays,
-        rankingsThirtyDays,
-      }}
-    >
-      {children}
-    </DataContext.Provider>
-  );
+	return (
+		<DataContext.Provider
+			value={{
+				NFTsData,
+				collectionsData,
+				rankingsOneDay,
+				rankingsSevenDays,
+				rankingsThirtyDays,
+			}}
+		>
+			{children}
+		</DataContext.Provider>
+	);
 }
