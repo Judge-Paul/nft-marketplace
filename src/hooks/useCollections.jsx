029b7e1ded32fb01c94f62aaf799e5c9b02ab767
Judge-Paul <judgepaulogebe3@gmail.com>
2024-07-28 15:36:11 +0100

    chore: update rankings page
    update useCollection hook to have sortBy param to get differenct collections based on date, update rankings page to have max width and set clicking on a collection to go to that collection's page

--- src/hooks/useCollections.jsx
+++ src/hooks/useCollections.jsx
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { get } from "../libs/api";
 
-const useCollections = () =>
+const useCollections = (sortBy = "allTimeVolume") =>
 	useQuery({
-		queryKey: ["collections"],
+		queryKey: ["collections", sortBy],
 		queryFn: async () => {
-			const data = await get("/collections/v7");
+			const data = await get(`/collections/v7?sortBy=${sortBy}`);
 			if (data.data) {
 				return data.data.collections;
 			}
