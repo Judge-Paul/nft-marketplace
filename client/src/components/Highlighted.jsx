006fe0bb4dadaa9c69275b7c2670e369de5557d7
Judge-Paul <judgepaulogebe3@gmail.com>
2024-12-25 23:28:09 +0100

    feat: use actual nft for highlighted section

--- client/src/components/Highlighted.jsx
+++ client/src/components/Highlighted.jsx
@@ -1,23 +1,44 @@
 import React from "react";
-import icon from "../assets/artists-avatars/Animakid.webp";
 import { motion } from "framer-motion";
 import { HiOutlineEye } from "react-icons/hi2";
 import Timer from "./Timer";
+import useTokens from "../hooks/useTokens";
 
 export default function Highlighted() {
+	const { data } = useTokens();
+
+	const token = data[3].token;
+
 	return (
 		<div className="bg-[#A259FF]">
-			<div className="bg-highlightedBg bg-cover bg-center text-white opacity-70 font-workSans py-6 sm:pt-80 pb-12 mb-20">
+			<div
+				className="bg-cover bg-center text-white font-workSans opacity-85 py-6 sm:pt-80 pb-12 mb-20"
+				style={{
+					backgroundImage: `url(${
+						token?.imageLarge ??
+						token?.image ??
+						"https://pbs.twimg.com/media/FrcERKSXwAcUwJ9?format=jpg&name=large"
+					})`,
+				}}
+			>
 				<div className="mx-auto max-w-7xl flex flex-wrap justify-around 2xl:justify-between">
-					<div className="mt-7">
+					<div className="mt-7 flex flex-col items-center sm:items-start">
 						<div className="flex bg-[#3B3B3B] w-max px-5 py-2 rounded-full">
-							<img src={icon} width="30px" alt="Animakid Icon" />
-							<p className="pl-4 my-auto font-medium">Animakid</p>
+							<img
+								src={token.collection.image}
+								width="30px"
+								className="rounded-full aspect-square"
+								alt={`${token?.collection?.name || "collection"} image`}
+							/>
+							<p className="pl-4 my-auto font-medium">
+								{(token?.collection?.name).slice(0, 20) || "Collection"}
+							</p>
 						</div>
-						<h4 className="text-3xl text-[2.2rem] lg:text-[3.2rem] font-workSans font-bold mt-7">
-							Magic Mashrooms
+						<h4 className="text-3xl truncate text-[2.2rem] lg:text-[3.2rem] font-workSans font-bold mt-7">
+							{(token?.name).slice(0, 15) || "NFT"}
 						</h4>
-						<motion.button
+						<motion.a
+							href={`https://opensea.io/assets/ethereum/${token.contract}/${token.tokenId}`}
 							className="flex bg-transparent py-4 px-12 rounded-2xl font-semibold mt-8 text-black bg-white"
 							whileHover={{ scale: 0.92 }}
 						>
@@ -26,7 +47,7 @@ export default function Highlighted() {
 								size="20px"
 							/>
 							See NFT
-						</motion.button>
+						</motion.a>
 					</div>
 					<div className="mt-7 md:mt-auto order-1 md:order-2">
 						<Timer hour={24} />
