1b3b06e7e48a1b7dc556549c1727b25b170b461c
Judge-Paul <judgepaulogebe3@gmail.com>
2024-12-28 20:47:10 +0100

    chore: use collection loading state as fallback while lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ErrorPage from "./pages/ErrorPage";
 import { Helmet } from "react-helmet";
 import ReactGA from "react-ga4";
+import HomePage from "./pages/HomePage";
+import CollectionLoading from "./components/CollectionLoading";
 
 const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
 
@@ -17,7 +19,6 @@ ReactGA.initialize(TRACKING_ID);
 
 const queryClient = new QueryClient();
 
-const HomePage = lazy(() => import("./pages/HomePage"));
 const MarketplacePage = lazy(() => import("./pages/MarketplacePage"));
 const ConnectPage = lazy(() => import("./pages/ConnectPage"));
 const RegisterPage = lazy(() => import("./pages/RegisterPage"));
@@ -26,6 +27,12 @@ const RankingsPage = lazy(() => import("./pages/RankingsPage"));
 const CollectionPage = lazy(() => import("./pages/CollectionPage"));
 const SearchPage = lazy(() => import("./pages/SearchPage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
+
+const WithSuspense = ({ children, isCollection }) => (
+	<Suspense fallback={isCollection ? <CollectionLoading /> : <Spinner />}>
+		{children}
+	</Suspense>
+);
 
 function App() {
 	function ScrollToTop() {
@@ -53,35 +60,87 @@ function App() {
 					<AuthProvider>
 						<Navbar />
 						<ScrollToTop />
-						<Suspense fallback={<Spinner />}>
-							<Routes>
-								<Route
-									path="/"
-									element={<HomePage />}
-									errorElement={<ErrorPage />}
-								/>
-								<Route path="/marketplace" element={<MarketplacePage />} />
-								<Route
-									path="/rankings"
-									element={<RankingsPage />}
-									errorElement={<ErrorPage />}
-								/>
-								<Route path="/connect" element={<ConnectPage />} />
-								<Route path="/register" element={<RegisterPage />} />
-								<Route path="/login" element={<LoginPage />} />
-								<Route
-									path="/collection/:slug"
-									element={<CollectionPage />}
-									errorElement={<ErrorPage />}
-								/>
-								<Route
-									path="/search"
-									element={<SearchPage />}
-									errorElement={<ErrorPage />}
-								/>
-								<Route path="*" element={<NotFoundPage />} />
-							</Routes>
-						</Suspense>
+						{/* <Suspense fallback={<Spinner />}> */}
+						<Routes>
+							<Route
+								path="/"
+								element={<HomePage />}
+								errorElement={<ErrorPage />}
+							/>
+							<Route
+								path="/marketplace"
+								element={
+									<WithSuspense>
+										<MarketplacePage />
+									</WithSuspense>
+								}
+								errorElement={<ErrorPage />}
+							/>
+							<Route
+								path="/rankings"
+								element={
+									<WithSuspense>
+										<RankingsPage />
+									</WithSuspense>
+								}
+								errorElement={<ErrorPage />}
+							/>
+							<Route
+								path="/connect"
+								element={
+									<WithSuspense>
+										<ConnectPage />
+									</WithSuspense>
+								}
+								errorElement={<ErrorPage />}
+							/>
+							<Route
+								path="/register"
+								element={
+									<WithSuspense>
+										<RegisterPage />
+									</WithSuspense>
+								}
+								errorElement={<ErrorPage />}
+							/>
+							<Route
+								path="/login"
+								element={
+									<WithSuspense>
+										<LoginPage />
+									</WithSuspense>
+								}
+								errorElement={<ErrorPage />}
+							/>
+							<Route
+								path="/search"
+								element={
+									<WithSuspense>
+										<SearchPage />
+									</WithSuspense>
+								}
+								errorElement={<ErrorPage />}
+							/>
+							<Route
+								path="/collection/:slug"
+								element={
+									<WithSuspense isCollection>
+										<CollectionPage />
+									</WithSuspense>
+								}
+								errorElement={<ErrorPage />}
+							/>
+							<Route
+								path="*"
+								element={
+									<WithSuspense>
+										<NotFoundPage />
+									</WithSuspense>
+								}
+								errorElement={<ErrorPage />}
+							/>
+						</Routes>
+						{/* </Suspense> */}
 						<Footer />
 					</AuthProvider>
 				</QueryClientProvider>
